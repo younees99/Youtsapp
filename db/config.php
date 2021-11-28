f3ae4a14bfaf5f72601e860dd2bedbec77987ea9
Younes <younesmalki213@gmail.com>
2021-11-28 16:20:03 +0100

    update

--- db/config.php
+++ db/config.php
@@ -1,6 +1,6 @@
 <?php    	
 	//Creating database for first time use	
-	$link = mysqli_connect("127.0.0.1", "root", "");
+	$link = mysqli_connect("127.0.0.1", "root");
 
     // Check connection
     if (!$link)
